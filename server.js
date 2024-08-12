ad705a9883112d278e3449a22d9b2bd4189e7ef1
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2024-08-12 08:55:36 -0300

    lista de usuarios e pegar usuarios com query

--- server.js
+++ server.js
@@ -3,11 +3,38 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const rafael = {
-    nome: "rafael",
-    idade: 27,
-    fruta: "caju"
-}
+const usuarios = [
+    {
+        "nome": "Ana Silva",
+        "email": "ana.silva@email.com",
+        "idade": 28,
+        "fruta": "maçã"
+    },
+    {
+        "nome": "João Pereira",
+        "email": "joao.pereira@email.com",
+        "idade": 34,
+        "fruta": "banana"
+    },
+    {
+        "nome": "Maria Oliveira",
+        "email": "maria.oliveira@email.com",
+        "idade": 22,
+        "fruta": "laranja"
+    },
+    {
+        "nome": "Carlos Santos",
+        "email": "carlos.santos@email.com",
+        "idade": 45,
+        "fruta": "manga"
+    },
+    {
+        "nome": "Beatriz Costa",
+        "email": "beatriz.costa@email.com",
+        "idade": 31,
+        "fruta": "uva"
+    }
+]
 
 
 app.get(`/`, (req, res) => {
@@ -19,7 +46,8 @@ app.get(`/caju`, (req, res) => {
 });
 
 app.get(`/pegarUsuario`, (req, res) => {
-    res.send(rafael);
+    const userEmail = req.query.email;
+    res.send(`O email requisitado é: ${userEmail}`)
 });
 
 
